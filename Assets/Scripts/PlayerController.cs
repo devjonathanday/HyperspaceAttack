b4c1d17737efa37bb73e607f8e35ae33cdddda47
jday151r <j.day151r@gmail.com>
2019-10-16 17:28:27 -0700

    Added Rewired package

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Rewired;
 
 public class PlayerController : MonoBehaviour
 {
     [Header("Input")]
     private Vector2 mouseDelta;
+    public Vector2 inputVector;
     public float lookSpeed;
+    public bool invertYAxis;
 
-    [Header("References - Self")]
-    public Transform cam;
+    [Header("Physics")]
+    public Rigidbody rBody;
+    public float moveSpeed;
+
+    [Header("References")]
+    private Player player;
+
+    void Awake()
+    {
+        player = ReInput.players.GetPlayer(0);
+    }
 
     void Start()
     {
@@ -20,10 +32,17 @@ public class PlayerController : MonoBehaviour
     {
         #region Input
 
+        //Camera Movement
         mouseDelta.x = Input.GetAxis("Mouse X");
         mouseDelta.y = Input.GetAxis("Mouse Y");
 
-        cam.Rotate(-mouseDelta.y * lookSpeed, mouseDelta.x * lookSpeed, 0);
+        transform.Rotate(mouseDelta.y * (invertYAxis ? lookSpeed : -lookSpeed), mouseDelta.x * lookSpeed, 0);
+
+        //Flying Movement
+        inputVector.x = player.GetAxis("Horizontal");
+        inputVector.y = player.GetAxis("Vertical");
+
+        rBody.AddRelativeForce(new Vector3(inputVector.x, 0, inputVector.y).normalized * moveSpeed);
 
         #endregion
     }
