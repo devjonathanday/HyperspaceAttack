5cae84a12534653c9b6a1ebac87656fc0d3134a7
Matt Dowling <mjd117@gmail.com>
2019-10-17 16:31:39 -0700

    Created Health Bars and bound them to player health component.

---
+++ Assets/Scripts/MattScripts/S_HealthCanvas.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_HealthCanvas : MonoBehaviour
+{
+
+    public GameObject[] HealthBars;
+
+    private int CurrentHealthBar = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+
+    public void PlayerHasTakenDamage()
+    {
+        HealthBars[CurrentHealthBar].SetActive(false);
+        CurrentHealthBar++;
+        if(CurrentHealthBar == 3)
+        {
+            Debug.Log("Dead");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/MattScripts/S_HealthComponent.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_HealthComponent : MonoBehaviour
+{
+
+    public int MaximumHealth;
+
+
+    public Canvas MyCanvas;
+    private int CurrentHealth;
+
+
+    private S_HealthCanvas MyCanvasHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentHealth = MaximumHealth;
+        MyCanvasHealth = MyCanvas.GetComponent<S_HealthCanvas>();
+    }
+
+
+
+    void TakeDamage(int DamageToTake)
+    {
+
+        CurrentHealth = CurrentHealth - DamageToTake;
+        MyCanvasHealth.PlayerHasTakenDamage();
+        if (CurrentHealth <= 0)
+            Die();
+
+    }
+
+    void Die()
+    {
+        Debug.Log("Get FUCKED IDIOT");
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T)) 
+        {
+            TakeDamage(1);
+            Debug.Log("Shit shit shit fuck");
+        }
+            
+    }
+
+}
