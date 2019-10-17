12cb17b85b4c233da343b977d1ebadf8060bc04e
Matt Dowling <mjd117@gmail.com>
2019-10-16 17:57:00 -0700

    Shield zone prefab and script setup

--- Assets/Scripts/S_ShieldZone.cs
+++ Assets/Scripts/S_ShieldZone.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class S_ShieldZone : MonoBehaviour
 {
+
+    public float TimeToDecrease;//Time until zone disappears
+    public Vector3 ShrinkSpeed; //How much to shrink the scale every tick
+
+
+
+    private bool CharacterInside = true;
+    private float CurrentTime;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
+        CurrentTime = TimeToDecrease;
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.other.tag == "PlayerCharacter")
+            CharacterInside = true;
         
     }
 
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.other.tag == "PlayerCharacter")
+            CharacterInside = false; 
+    }
+
+
+    void ShrinkZone()
+    {
+        if (CharacterInside)
+        {
+            transform.localScale -= new Vector3(Time.deltaTime,0,Time.deltaTime);
+            if (transform.localScale.x <= 0)
+                Destroy(gameObject);
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        ShrinkZone();
     }
+
+    
+
+
+    
 }
