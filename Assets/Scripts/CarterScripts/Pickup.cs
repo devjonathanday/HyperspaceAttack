2474e62a56fd45c94245d750de5cfa5896c67ba8
Carter <carter.patterson95@gmail.com>
2019-10-16 17:51:54 -0700

    AI
    blibbity blobbity give em the sloppity

---
+++ Assets/Scripts/CarterScripts/Pickup.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    enum Type { point, damageBoost }
+    uint ID { get; set; }
+    Type type { get; set; }
+}
