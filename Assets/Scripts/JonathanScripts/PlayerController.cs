61aa5a60fd7564ff8e4a16093ed833e2d9e8dd83
jday151r <j.day151r@gmail.com>
2019-10-17 00:02:34 -0700

    Disabled aiming at bullets, adjusted lighting

--- Assets/Scripts/JonathanScripts/PlayerController.cs
+++ Assets/Scripts/JonathanScripts/PlayerController.cs
@@ -16,8 +16,13 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;
 
     [Header("Shooting")]
+    public bool canShoot;
     public float shotRange; //Distance of raycast from camera to hit point
     public float bulletSpeed;
+    [Space(10)]
+    public bool autoFire;
+    float fireTimeStamp;
+    [SerializeField] float fireDelay;
 
     [Header("Visuals")]
     public float boostShakeThreshold;
@@ -59,13 +64,13 @@ public class PlayerController : MonoBehaviour
         if (inputVector.y > boostShakeThreshold)
             cam.ScreenShake(boostShakeIntensity, boostShakeDuration);
 
-        rBody.AddRelativeForce(inputVector.normalized * moveSpeed);
+        rBody.AddRelativeForce(inputVector.normalized * moveSpeed, ForceMode.Acceleration);
 
         #endregion
 
         #region Shooting
 
-        if (player.GetButtonDown("Fire1"))
+        if (autoFire ? (player.GetButton("Fire1") && Time.time - fireTimeStamp > fireDelay) : player.GetButtonDown("Fire1"))
             ShootProjectile();
 
         #endregion
@@ -74,15 +79,22 @@ public class PlayerController : MonoBehaviour
     {
         Ray trajectory = new Ray(transform.position, transform.forward);
         if (Physics.Raycast(trajectory, out RaycastHit hit, shotRange))
-            return hit.point;
-        else return transform.position + (transform.forward * shotRange);
+        {
+            if(!hit.collider.gameObject.CompareTag("Bullet")) //Disable aiming at bullets
+                return hit.point;
+        }
+        return transform.position + (transform.forward * shotRange);
     }
 
     void ShootProjectile()
     {
-        GameObject newBullet = Instantiate(projectileBullet, firePoint.position, Quaternion.identity);
-        Vector3 trajectory = GetBulletTarget() - firePoint.position;
-        newBullet.GetComponent<Rigidbody>().AddForce(trajectory.normalized * bulletSpeed);
+        if (canShoot)
+        {
+            fireTimeStamp = Time.time;
+            GameObject newBullet = Instantiate(projectileBullet, firePoint.position, Quaternion.identity);
+            Vector3 trajectory = GetBulletTarget() - firePoint.position;
+            newBullet.GetComponent<Rigidbody>().AddForce((trajectory.normalized * bulletSpeed) + rBody.velocity);
+        }
     }
 
 }
