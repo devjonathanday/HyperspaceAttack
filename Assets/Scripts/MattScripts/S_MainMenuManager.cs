de8c61c422eb98409555c5bf0184f1c3ad13b1b0
Matt Dowling <mjd117@gmail.com>
2019-10-18 11:19:53 -0700

    Created MainMenuManager Script

---
+++ Assets/Scripts/MattScripts/S_MainMenuManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_MainMenuManager : MonoBehaviour
+{
+
+    
+    
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+
+    void StartButtonPressed()
+    {
+
+    }
+
+    void QuitButtonPressed()
+    {
+        Application.Quit();
+    }
+
+    void ControlButtonPressed()
+    {
+
+    }
+
+    void MouseKeyboardButtonPressed()
+    {
+       
+    }
+
+    void MainMenuButtonPressed()
+    {
+
+    }
+}
+
+   
+
